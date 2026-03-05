8299e6929a5ba0cfc7be7b1b7eeeadb182d71212
Rishabh Thander <thanderrishabh@gmail.com>
2026-03-05 15:18:08 +0530

    switch code.c

---
+++ switch code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int x;
+    printf("enter x :");
+    scanf("%d",&x);
+    switch(x){
+        case 1:
+        printf("monday\n");
+        break;
+        case 2:printf("sunday\n");
+        break;
+    }
+    return 0;
+}
